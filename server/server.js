210afde0336ea198040bed12213a0a2cdd956a96
otakuchan <ianbjs@gmail.com>
2022-10-26 05:49:42 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+
+const startApolloServer = async () => {
+    const app = express();
+    
+    const httpServer = http.createServer(app);
+    
+    const server = new ApolloServer({
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+    });
+
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+};
+
+startApolloServer();
