386aca43082160181684f8f83ddfdc9aa8efaeca
otakuchan <ianbjs@gmail.com>
2022-10-27 10:38:11 -0700

    Chore: clean up code

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,6 @@
 import { gql } from "apollo-server-core";
 import { people, cars } from "./peopleCarsScheme";
-import { find, remove } from 'lodash'
+import { find } from 'lodash'
 
 const typeDefs = gql`
 	type Person {
@@ -45,7 +45,7 @@ const resolvers = {
                 return person
 			});
 		},
-		person: (parent, args, context, info) => {
+		person: (args) => {
             const person = find(people, { id: args.id })
 
             return {
@@ -54,13 +54,13 @@ const resolvers = {
             }
 		},
 		cars: () => cars,
-		car: (parent, args, context, info) => {
+		car: (args) => {
             return find(cars, { id: args.id })
 		},
 	},
     Mutation: {
         // People
-        addPerson: (root, args) => {
+        addPerson: (args) => {
             const newPerson = {
                 id: args.id,
                 firstName: args.firstName,
@@ -70,13 +70,12 @@ const resolvers = {
             people.push(newPerson);
             return newPerson;
         },
-        removePerson: (root, args) => {
+        removePerson: (args) => {
             const person = find(people, { id: args.id })
             people.splice(people.indexOf(person), 1);
-            // cars = cars.filter(car => car.personId !== person.id);
             return person;
         },
-        updatePerson: (root, args) => {
+        updatePerson: (args) => {
             const person = find(people, { id: args.id })
             person.firstName = args.firstName;
             person.lastName = args.lastName;
@@ -84,12 +83,12 @@ const resolvers = {
         },
 
         // Cars
-        removeCar: (root, args) => {
+        removeCar: (args) => {
             const car = find(cars, { id: args.id })
             cars.splice(cars.indexOf(car), 1);
             return car;
         },
-        addCar: (root, args) => {
+        addCar: (args) => {
             const newCar = {
                 id: args.id,
                 year: args.year,
@@ -102,7 +101,7 @@ const resolvers = {
             cars.push(newCar);
             return newCar;
         },
-        updateCar: (root, args) => {
+        updateCar: (args) => {
             const car = find(cars, { id: args.id })
             car.year = args.year;
             car.make = args.make;
