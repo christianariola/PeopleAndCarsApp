735f2d01b67a7cc2d99f10425c35c017ef581879
otakuchan <ianbjs@gmail.com>
2022-10-27 00:42:46 -0700

    Add: mutation for adding person and car

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,5 +1,6 @@
 import { gql } from "apollo-server-core";
 import { people, cars } from "./peopleCarsScheme";
+import { find, remove } from 'lodash'
 
 const typeDefs = gql`
 	type Person {
@@ -9,32 +10,58 @@ const typeDefs = gql`
 		cars: [Car]
 	}
 
-    type Car {
-        id: ID!
-        year: String!
-        make: String!
-        model: String!
-        price: String!
-        personId: String!
-    }
+	type Car {
+		id: ID!
+		year: String!
+		make: String!
+		model: String!
+		price: String!
+		personId: String!
+	}
 
-    type Query {
-        people: [Person]
-        person(id: ID!): Person
-        cars: [Car]
-        car(id: ID!): Car
+	type Query {
+		people: [Person]
+		person(id: ID!): Person
+		cars: [Car]
+		car(id: ID!): Car
+	}
+
+    type Mutation {
+        addPerson(firstName: String!, lastName: String!): Person
+        addCar(year: String!, make: String!, model: String!, price: String!, personId: String!): Car
     }
-`
+`;
 
 const resolvers = {
-    Query: {
-        people: () => people,
-        person: (parent, args, context, info) => {
-            return people.find(person => person.id === args.id)
-        },
-        cars: () => cars,
-        car: (parent, args, context, info) => {
-            return cars.find(car => car.id === args.id)
+	Query: {
+		people: () => {
+			return people.map((person) => {
+				person.cars = cars.filter((car) => car.personId === person.id);
+                return person
+			});
+		},
+		person: (parent, args, context, info) => {
+			const person = people.find((person) => person.id === args.id);
+
+            return {
+                ...person,
+                cars: cars.filter((car) => car.personId === person.id)
+            }
+		},
+		cars: () => cars,
+		car: (parent, args, context, info) => {
+            return find(cars, { id: args.id })
+		},
+	},
+    Mutation: {
+        addPerson: (parent, args, context, info) => {
+            const person = {
+                id: people.length + 1,
+                firstName: args.firstName,
+                lastName: args.lastName
+            }
+            people.push(person);
+            return person;
         }
     }
 };
